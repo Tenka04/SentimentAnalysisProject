db3df48c3a7a2294e35a396015e06805e0d03317
Sujal Sangle <sujalsangle@gmail.com>
2025-06-04 00:37:34 +0530

    finalized analysis plots

--- SAP.py
+++ SAP.py
@@ -12,9 +12,9 @@ import nltk
 df = pd.read_csv('input/amazon-food-review/Reviews.csv')
 
 #Display graphical representation of the first 5 rows
-ax = df['Score'].value_counts().sort_index().plot(kind='bar', title='Count of Reviews by stars', figsize=(10, 5))
-ax.set_xlabel('Review Stars')
-##plt.show()
+#ax = df['Score'].value_counts().sort_index().plot(kind='bar', title='Count of Reviews by stars', figsize=(10, 5))
+#ax.set_xlabel('Review Stars')
+#plt.show()
 
 from nltk.sentiment import SentimentIntensityAnalyzer
 from tqdm import tqdm
@@ -34,3 +34,17 @@ vaders = vaders.merge(df, how='left')
 
 #seniment score and meta data
 print(vaders.head())
+
+#PLOT VADER RESULTS
+ax = sns.barplot(data=vaders, y='pos', x='Score')
+ax.set_title('Vader Compound Score by Review Stars')
+plt.show()
+
+fig, ax = plt.subplots(1, 3, figsize=(15, 3))
+sns.barplot(data=vaders, y='pos', x='Score', ax=ax[0])
+sns.barplot(data=vaders, y='neu', x='Score', ax=ax[1])
+sns.barplot(data=vaders, y='neg', x='Score', ax=ax[2])
+ax[0].set_title('Positive')
+ax[1].set_title('Neutral')
+ax[2].set_title('Negative')
+plt.show()
